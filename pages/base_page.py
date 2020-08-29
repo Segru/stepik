c6150be8bff19ce022e3983dae3f3055cf57d1bb
Sergey <s.gruchik@ispsystem.com>
2020-08-29 18:24:10 +0800

    adding classes

--- pages/base_page.py
+++ pages/base_page.py
@@ -5,12 +5,21 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
 
+from .locators import BasePageLocators
+
 class BasePage:
     def __init__(self, browser: webdriver, url, timeout=0):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
 
+    def go_to_login_page(self):    
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), 'Login link is not presented'
+    
     def open(self):
         self.browser.get(self.url)
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,18 +1,9 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 import pytest
 import time
 
-# @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-#                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-#                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
 @pytest.mark.parametrize('link', [link])
 def test_add_product_to_basket(browser, link):
@@ -51,3 +42,23 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.solve_quiz_and_get_code()
     page.should_success_message_disappeare()
 
+@pytest.mark.parametrize('link', [link])
+def test_guest_should_see_login_link_on_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.parametrize('link', [link])
+def test_guest_can_go_to_login_page_from_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+
+@pytest.mark.parametrize('link', [link])
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser, link):
+
+
+@pytest.mark.parametrize('link', [link])
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, link):
